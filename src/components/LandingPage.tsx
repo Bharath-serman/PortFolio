999471409d7fc6f18e3025845a42e3fa7a2a4cfe
Bharath-serman <bharathserman@gmail.com>
2026-01-07 12:40:12 +0530

    Updated LandingPage

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -19,18 +19,18 @@ const LandingPage: React.FC<{ onContinue: () => void }> = ({ onContinue }) => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-white relative overflow-hidden bg-black">
       <div className="comet-shower absolute top-0 left-0  w-full h-full z-0 pointer-events-none" />
-      <h1 className="text-5xl font-bold mb-4 text-shadow">Welcome to My Portfolio</h1>
-      <p className="mb-8 text-lg text-shadow">Exploring Unity Engine | Game Development | User Interface Design</p>
+      <h1 className="text-5xl font-bold mb-4 text-shadow">Bharath serman R</h1>
+      <p className="mb-8 text-lg text-shadow">Turning Code into Playable Experiences</p>
       <button
         onClick={onContinue}
-        className="py-3 px-8 bg-orange-400 hover:bg-orange-500 rounded-full text-white font-medium inline-flex items-center transition-all duration-300 hover:shadow-lg"
+        className="py-3 px-8 bg-orange-400 hover:bg-orange-300 rounded-full text-white font-medium inline-flex items-center transition-all duration-300 hover:shadow-lg"
       >
-        Enter Portfolio
+        Start Exploring
       </button>
       <style>
   {`
     .text-shadow {
-      text-shadow: 0 0 15px rgba(0, 183, 255, 0.5), 0 0 30px rgba(14, 183, 250, 0.7);
+      text-shadow: 0 0 15px rgba(255, 0, 0, 0.5), 0 0 30px rgba(250, 14, 14, 0.7);
     }
 
     .comet-shower {
